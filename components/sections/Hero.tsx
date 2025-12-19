66250132fde6f4b801a98f6a27fd125355120edb
Aryantvats <tyagiaryan469@gmail.com>
2025-12-19 15:51:04 +0530

    moon positioning

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -3,9 +3,8 @@ import Moon from "@/components/sections/Moon";
 
 const Hero = () => {
   return (
-    <section className="min-h-screen flex items-center px-6 md:px-20 text-white">
-      <div className="max-w-7xl ">
-        
+    <section className="relative min-h-screen flex items-center px-6 md:px-20 text-white ">
+      <div className="max-w-7xl">
         <h1 className="text-4xl sm:text-5xl md:text-[80px] font-extrabold leading-tight uppercase">
           Let’s travel to the edge <br /> of space
         </h1>
@@ -18,9 +17,19 @@ const Hero = () => {
         <button className="h-[50px] mt-7.5 px-6 py-3 border border-white text-[14px] tracking-widest uppercase hover:bg-white hover:text-black transition">
           Discover More
         </button>
-          </div>
-          
-       <Moon top="470px" right="148px" className="z-50 hidden"/>
+      </div>
+
+      <Moon
+        className="
+          absolute
+          bottom-[-260px]
+          right-[148px]
+          hidden
+          lg:block
+          z-50
+        "
+        size={360}
+      />
     </section>
   );
 };
